e0681d3d545bc8fd0028176d9709c7d04c160f26
Nienke Schuuring (2504426) <2504426schuuring@zuyd.nl>
2026-07-02 20:18:30 +0200

    prijzen ook aan besteloverzicht toegevoegd. plus layout bestel overzicht nog iets veranderd

--- KE03_INTDEV_SE_1_Base/Pages/Bestellen.cshtml.cs
+++ KE03_INTDEV_SE_1_Base/Pages/Bestellen.cshtml.cs
@@ -1,4 +1,6 @@
 using DataAccessLayer.Interfaces;
+using DataAccessLayer.Models;
+using DataAccessLayer.Repositories;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -7,11 +9,16 @@ namespace KE03_INTDEV_SE_1_Base.Pages
     public class BestellenModel : PageModel
     {
         private readonly ICustomerRepository _customerRepository;
+        private readonly IOrderRepository _orderRepository;
 
-        public BestellenModel(ICustomerRepository customerRepository)
+        public Order Order { get; set; }
+
+        public BestellenModel(ICustomerRepository customerRepository, IOrderRepository orderRepository)
         {
             _customerRepository = customerRepository;
+            _orderRepository = orderRepository;
         }
+        
 
         public IActionResult OnPostBetalen(int CustomerId)
         {
@@ -20,8 +27,23 @@ namespace KE03_INTDEV_SE_1_Base.Pages
             return RedirectToPage("/Betaalt");
         }
 
+        public decimal totaalbedrag { get; set; }
+        public decimal totaalVerzendBedrag { get; set; }
+
         public void OnGet()
         {
+            int orderId = 1;
+            Order = _orderRepository.GetOrderById(orderId);
+            decimal totaalbedrag = 0;
+            decimal totaalVerzendBedrag = 0;
+            foreach (var item in Order.Products)
+            {
+                var price = item.Price;
+                totaalbedrag += price;
+            }
+            this.totaalbedrag = totaalbedrag;
+            totaalVerzendBedrag = totaalbedrag + 3.95m;
+            this.totaalVerzendBedrag = totaalVerzendBedrag;
         }
     }
 }
